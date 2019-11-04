0167c8f404e5578918655a08def9641d5023bf5e
Giftsonj <50142321+Giftsonj@users.noreply.github.com>
2019-11-04 16:59:16 +0530

    Add files via upload

--- view-child.component.ts
+++ view-child.component.ts
@@ -1,34 +1,31 @@
 import { Component, OnInit, ViewChild, ElementRef, Input, EventEmitter, Output } from '@angular/core';
-// import { EmployeeService } from '../employee.service';
-import { EmployeeService } from 'app/employee.service';
-//import { EmployeeService } from '../employee.service'
+import { EmployeeServiceService } from '../employee-service.service';
+
 @Component({
   selector: 'app-view-child',
   templateUrl: './view-child.component.html',
   styleUrls: ['./view-child.component.css']
 })
 export class ViewChildComponent implements OnInit {
-  
-  @ViewChild('serverNametInput') serverContentInput: ElementRef;
+
+
+  @ViewChild('serverNametInput', { static: true }) serverContentInput: ElementRef;
   @Input('parentData') public name;
   @Output() public childEvent = new EventEmitter();
-  
+
   serverContent: string
   public exployees = [];
 
-  constructor(private empService: EmployeeService) { }
+  constructor(private empService: EmployeeServiceService) { }
 
   ngOnInit() {
     this.exployees = this.empService.getEmployees();
+
   }
 
   onAddServer(nameInput: HTMLInputElement) {
-    // console.log(this.serverContentInput.nativeElement.value)
-
     this.serverContent = this.serverContentInput.nativeElement.value;
   }
-
-
 
   childToParent() {
     this.childEvent.emit(this.name)
